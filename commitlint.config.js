aadae650ee5641146d84c031588d1e0365031b71
AMarcosCastelo <marcos-12marcos@hotmail.com>
2022-01-26 14:04:47 -0300

    chore: typescript

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,8 +1,3 @@
-// const vsCodeSettings = require('./.vscode/settings.json');
-
 module.exports = {
   extends: ['@commitlint/config-conventional'],
-  // rules: {
-  //   'scope-enum': [2, 'always', vsCodeSettings['conventionalCommits.scopes']],
-  // },
 };
